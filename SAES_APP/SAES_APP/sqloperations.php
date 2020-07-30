d6cb059755f5508f18d526f8f1925af575fe2317
lupitaesp <51250754+lupitaesp@users.noreply.github.com>
2020-07-30 02:25:16 -0500

    INSERCION DE REGISTRO

---
+++ SAES_APP/SAES_APP/sqloperations.php
@@ -0,0 +1,46 @@
+<?php
+ 
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "integradora_app";
+    $table = "persona"; 
+ 
+    //This command came from the app, you will see it soon 
+    $action = $_POST["action"];
+     
+    // Create Connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check Connection
+    if($conn->connect_error){
+        die("Connection Failed: " . $conn->connect_error);
+        return;
+    } 
+    // If connection is OK...
+    
+    //Save Data
+
+    if("INSERT_DATA" == $action){
+       //$cve_persona = $_POST["cve_personas"];
+       $matricula = $_POST["matricula"];
+       $nombre = $_POST["nombre"];
+       $ape_pat = $_POST["ape_pat"];
+       $ape_mat = $_POST["ape_mat"];
+       $email = $_POST["email"];
+       $carrera = $_POST["carrera"];
+       $grupo = $_POST["grupo"];
+       $telefono = $_POST["telefono"];
+       $sexo = $_POST["sexo"];
+       $contrasena = $_POST["contrasena"];
+       $usuario = $_POST["usuario"];
+       $foto    = $_POST["foto"];
+       $sql = "INSERT INTO $table (matricula,nombre,ape_pat,ape_mat,email,carrera,grupo,telefono,sexo,contrasena,usuario,foto)
+       VALUES('$matricula','$nombre','$ape_pat','$ape_mat','$email','$carrera','$grupo','$telefono','$sexo','$contrasena','$usuario','$foto')";
+       $result = $conn->query($sql);
+       echo "success";            
+       $conn->close();
+       return;
+   }
+   //Update y Delete 
+
+    ?>
