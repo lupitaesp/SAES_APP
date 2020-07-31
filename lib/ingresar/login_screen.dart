67f3f5fe73f3d781cb0e97b8168771e4db027a0c
lupitaesp <51250754+lupitaesp@users.noreply.github.com>
2020-07-31 01:10:27 -0500

    LOGIN FUNCIONANDO, FALTAN PANTALLAS DEL HOME

--- lib/ingresar/login_screen.dart
+++ lib/ingresar/login_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
 
 class LoginPage extends StatefulWidget {
   @override
@@ -9,6 +12,31 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  TextEditingController mail = new TextEditingController();
+  TextEditingController pass = new TextEditingController();
+
+  Future<List> _login() async {
+    final response =
+        await http.post("http://192.168.0.108/SAES_APP/login.php", body: {
+      "email": mail.text,
+      "contrasena": pass.text,
+    });
+    var datauser = json.decode(response.body);
+    if(datauser.length==0){
+      setState(() {
+        //Snackbar
+      });
+    }else{
+      if(datauser[0]['USUARIO']=='Alumno'){
+        print("BENVENIDO ALUMNO");
+      }else if(datauser[0]['USUARIO']=='Doctora'){
+        print("bienvenido doctora");
+      }
+    }
+  }
+
+  
+
   @override
   void initState() {
     SystemChrome.setEnabledSystemUIOverlays([]);
@@ -31,11 +59,9 @@ class _LoginPageState extends State<LoginPage> {
       body: Container(
         child: ListView(
           children: <Widget>[
-             new Padding(
-                      padding: const EdgeInsets.only(
-                        top: 3, right: 32
-                      ),
- ),
+            new Padding(
+              padding: const EdgeInsets.only(top: 3, right: 32),
+            ),
             Container(
               width: MediaQuery.of(context).size.width,
               height: MediaQuery.of(context).size.height / 3.5,
@@ -51,7 +77,6 @@ class _LoginPageState extends State<LoginPage> {
                   ),
                   borderRadius:
                       BorderRadius.only(bottomLeft: Radius.circular(90))),
-             
             ),
             new Padding(
               padding: const EdgeInsets.only(top: 40, right: 32),
@@ -64,16 +89,17 @@ class _LoginPageState extends State<LoginPage> {
                 children: <Widget>[
                   Container(
                     width: MediaQuery.of(context).size.width / 1.2,
-                    height: 45,
+                    height: 50,
                     padding:
                         EdgeInsets.only(top: 4, left: 16, right: 16, bottom: 4),
                     decoration: BoxDecoration(
                         borderRadius: BorderRadius.all(Radius.circular(50)),
                         color: Colors.white,
                         boxShadow: [
-                          BoxShadow(color: Colors.black12, blurRadius: 5)
+                          BoxShadow(color: Colors.black12, blurRadius: 10)
                         ]),
                     child: TextField(
+                      controller: mail,
                       keyboardType: TextInputType.emailAddress,
                       decoration: InputDecoration(
                         border: InputBorder.none,
@@ -98,6 +124,7 @@ class _LoginPageState extends State<LoginPage> {
                           BoxShadow(color: Colors.black12, blurRadius: 5)
                         ]),
                     child: TextField(
+                      controller: pass,
                       obscureText: true,
                       decoration: InputDecoration(
                         border: InputBorder.none,
@@ -114,8 +141,10 @@ class _LoginPageState extends State<LoginPage> {
                   ),
                   InkWell(
                     //Direccion de la pantalla de usuario
+
                     onTap: () {
-                      Navigator.pushNamed(context, '/');
+                      _login();
+                      print("FUNCIONANDO");
                     },
                     child: Container(
                       height: 45,
--- lib/principal.dart
+++ lib/principal.dart
@@ -21,67 +21,69 @@ class _Principal extends State<Principal>{
             onPressed: () { },
           ),
       ),
-      body: new Container(
-            decoration: BoxDecoration(color: Colors.white),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  new Padding(padding: EdgeInsets.all(30.0)),
-                  Text('Universidad Tecnológica de Tulancingo', style: TextStyle(fontSize: 23)),
-                  Image.asset('assets/Halcon.png'),
-                  new Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      new Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          IconButton(
-                            icon: Icon(Icons.keyboard_arrow_right, color:Colors.grey[600]),
-                              onPressed: (){},
-                      ),
-                      new Text('Seleccione una opción:', style: TextStyle(fontSize: 18)),
-                        ],
-                      ),
-                      new Divider(
-                        color: Colors.black,
-                        indent: 75,
-                        endIndent: 75,
-                        thickness: 2.0
-                      ),
-                    ],
-                  ),
-                  new Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      MaterialButton(
-                        textColor: Colors.white,
-                        color: Colors.green,
-                        child: new Text('    Registrar    '),
-                        onPressed: (){
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => Registro()),
-                          );
-                        },
-                        shape:
-                          RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
-                      ),
-                      MaterialButton(
-                        textColor: Colors.white,
-                        color: Colors.indigo,
-                        child: new Text('Iniciar Sesión'),
-                        onPressed: (){
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => LoginPage()),
-                          );
-                        },
-                        shape:
-                          RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
-                      ),
-                    ],
-                  ),
-          ],
+      body: SingleChildScrollView(
+        child: new Container(
+              decoration: BoxDecoration(color: Colors.white),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    new Padding(padding: EdgeInsets.all(30.0)),
+                    Text('Universidad Tecnológica de Tulancingo', style: TextStyle(fontSize: 23)),
+                    Image.asset('assets/Halcon.png'),
+                    new Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        new Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            IconButton(
+                              icon: Icon(Icons.keyboard_arrow_right, color:Colors.grey[600]),
+                                onPressed: (){},
+                        ),
+                        new Text('Seleccione una opción:', style: TextStyle(fontSize: 18)),
+                          ],
+                        ),
+                        new Divider(
+                          color: Colors.black,
+                          indent: 75,
+                          endIndent: 75,
+                          thickness: 2.0
+                        ),
+                      ],
+                    ),
+                    new Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        MaterialButton(
+                          textColor: Colors.white,
+                          color: Colors.green,
+                          child: new Text('    Registrar    '),
+                          onPressed: (){
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => Registro()),
+                            );
+                          },
+                          shape:
+                            RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
+                        ),
+                        MaterialButton(
+                          textColor: Colors.white,
+                          color: Colors.indigo,
+                          child: new Text('Iniciar Sesión'),
+                          onPressed: (){
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) => LoginPage()),
+                            );
+                          },
+                          shape:
+                            RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
+                        ),
+                      ],
+                    ),
+            ],
+          ),
         ),
       )
     );
